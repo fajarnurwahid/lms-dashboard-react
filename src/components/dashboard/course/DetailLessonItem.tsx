14f48986230c86b67ebe8b98406e81936ca345d5
Fajar Nur Wahid <fajarnurwahid12@gmail.com>
2024-07-17 11:49:22 +0700

    Added course detail page

--- src/components/dashboard/course/DetailLessonItem.tsx
+++ src/components/dashboard/course/DetailLessonItem.tsx
@@ -1,48 +1,113 @@
+import { ChevronRight, Video, File } from "lucide-react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { ChevronRight, Lock } from "lucide-react";
+import { twMerge } from "tailwind-merge";
 
-type DetailLessonItemProps = {
+type DetailLessonItemProps = React.HTMLAttributes<HTMLDivElement> & {
+    children: React.ReactNode;
+};
+type DetailLessonItemTriggerProps = {
+    label: string;
+    duration: string;
+    lesson: number;
+};
+type DetailLessonItemLessonItemProps = {
+    type?: "video" | "file";
     title: string;
-    link: string;
-    description: string;
-    isLocked?: boolean;
+    duration: string;
+    hasPreview?: boolean;
+    previewLink?: string;
 };
 
-export default function DetailLessonItem({
-    title,
-    link,
-    description,
-    isLocked = true,
+export function DetailLessonItem({
+    children,
+    className,
+    ...props
 }: DetailLessonItemProps) {
     return (
-        <div className="flex items-center justify-between space-x-4 py-3">
-            <div className="flex items-center space-x-4">
-                <img
-                    src="https://github.com/shadcn.png"
-                    alt=""
-                    className="w-16 h-16 object-cover block rounded"
+        <div
+            className={twMerge("group/lesson-section", className)}
+            data-lesson-section
+            {...props}
+        >
+            {children}
+        </div>
+    );
+}
+
+export function DetailLessonItemTrigger({
+    label,
+    duration,
+    lesson,
+}: DetailLessonItemTriggerProps) {
+    function handleTriggerClick(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault();
+        e.currentTarget
+            .closest("[data-lesson-section]")
+            ?.classList.toggle("active");
+    }
+
+    return (
+        <button
+            type="button"
+            className="w-full text-left bg-neutral-50 dark:bg-neutral-950 px-4 md:px-6 flex items-center h-10"
+            onClick={handleTriggerClick}
+        >
+            <ChevronRight
+                size={16}
+                className="mr-2 flex-shrink-0 group-[.active]/lesson-section:rotate-90"
+            />
+            <span className="font-semibold min-w-0 truncate mr-2">{label}</span>
+            <span className="text-xs text-neutral-700 dark:text-neutral-300 ml-auto">
+                {lesson} lesson{lesson > 1 && "s"} • {duration}
+            </span>
+        </button>
+    );
+}
+
+export function DetailLessonItemLesson({ children }: DetailLessonItemProps) {
+    return (
+        <div className="px-4 md:px-6 py-4 hidden group-[.active]/lesson-section:block">
+            <div className="ml-6 space-y-3">{children}</div>
+        </div>
+    );
+}
+
+export function DetailLessonItemLessonItem({
+    type = "video",
+    title,
+    duration,
+    hasPreview = false,
+    previewLink = "/",
+}: DetailLessonItemLessonItemProps) {
+    return (
+        <div className="flex items-center">
+            {type === "file" ? (
+                <File
+                    size={16}
+                    className="mr-3 text-neutral-500 flex-shrink-0"
                 />
-                <div>
+            ) : (
+                <Video
+                    size={16}
+                    className="mr-3 text-neutral-500 flex-shrink-0"
+                />
+            )}
+            <p className="text-sm text-neutral-700 dark:text-neutral-300 font-medium truncate min-w-0 mr-4">
+                {title}
+            </p>
+            <div className="flex items-center space-x-4 ml-auto">
+                {hasPreview && (
                     <Link
-                        to={link}
-                        className="text-lg leading-tight font-semibold text-indigo-600 hover:underline inline-block mb-1"
+                        to={previewLink}
+                        className="text-sm text-indigo-600 dark:text-indigo-400 font-medium hover:underline"
                     >
-                        {title}
-                    </Link>
-                    <p className="text-neutral-500 text-sm line-clamp-2">
-                        {description}
-                    </p>
-                </div>
-            </div>
-            <div className="flex-shrink-0">
-                {isLocked ? (
-                    <Lock size={16} className="text-neutral-500" />
-                ) : (
-                    <Link to="/" className="btn btn-primary btn-sm space-x-1">
-                        <span>Start</span>
-                        <ChevronRight size={14} />
+                        Preview
                     </Link>
                 )}
+                <span className="text-xs text-neutral-700 dark:text-neutral-300 ml-auto">
+                    {duration}
+                </span>
             </div>
         </div>
     );
--- src/routes/dashboard/CourseDetails.tsx
+++ src/routes/dashboard/CourseDetails.tsx
@@ -7,25 +7,26 @@ import {
     Facebook,
     Instagram,
     Twitter,
+    GraduationCap,
+    CheckCircle,
+    Play,
+    Target,
 } from "lucide-react";
 import { Card, CardBody, CardHeader } from "../../components/dashboard/ui/Card";
-import DetailLessonItem from "../../components/dashboard/course/DetailLessonItem";
-import DetailLesson from "../../components/dashboard/course/DetailLesson";
+import {
+    DetailLessonItem,
+    DetailLessonItemLesson,
+    DetailLessonItemLessonItem,
+    DetailLessonItemTrigger,
+} from "../../components/dashboard/course/DetailLessonItem";
 
 export default function CourseDetails() {
     return (
         <div className="p-4 md:p-6">
             <Card className="overflow-hidden mb-4 md:mb-6">
-                <div className="flex relative isolate">
-                    <div className="flex space-x-6 w-full p-4 md:p-6">
-                        <div className="flex-shrink-0">
-                            <img
-                                src="https://github.com/shadcn.png"
-                                alt=""
-                                className="w-64 h-56 object-cover rounded-md block"
-                            />
-                        </div>
-                        <div className="w-full">
+                <div className="lg:flex relative isolate">
+                    <div className="w-full p-4 md:p-6">
+                        <div className="mb-4">
                             <p className="mb-2 text-neutral-500 font-medium">
                                 A course by{" "}
                                 <Link
@@ -72,15 +73,29 @@ export default function CourseDetails() {
                                     (45,324 reviews)
                                 </span>
                             </div>
-                            <p className="text-neutral-500 line-clamp-2 leading-normal">
+                            <p className="text-neutral-500 leading-normal">
                                 Lorem ipsum dolor sit amet consectetur
                                 adipisicing elit. Officia error praesentium aut
                                 eaque velit voluptatum, nihil possimus beatae
                                 maiores. Debitis?
                             </p>
                         </div>
+                        <div className="flex items-center flex-wrap space-y-2 space-x-2 -ml-2 -mt-2">
+                            <div className="flex items-center space-x-2 text-xs font-medium first:ml-2 first:mt-2 h-6 px-3 rounded-full bg-neutral-100 dark:bg-neutral-950">
+                                <GraduationCap size={16} />
+                                <span>2,345 learners</span>
+                            </div>
+                            <div className="flex items-center space-x-2 text-xs font-medium first:ml-2 first:mt-2 h-6 px-3 rounded-full bg-neutral-100 dark:bg-neutral-950">
+                                <CheckCircle size={16} />
+                                <span>87% completions</span>
+                            </div>
+                            <div className="flex items-center space-x-2 text-xs font-medium first:ml-2 first:mt-2 h-6 px-3 rounded-full bg-neutral-100 dark:bg-neutral-950">
+                                <GraduationCap size={16} />
+                                <span>2,345 learners</span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="w-96 flex-shrink-0"></div>
+                    <div className="hidden lg:block w-80 xl:w-96 flex-shrink-0"></div>
                     <div className="w-1/2 absolute top-0 right-0 h-full after:absolute after:w-full after:h-full after:top-0 after:left-0 after:bg-gradient-to-r after:from-white dark:after:from-neutral-900 after:to-white/80 dark:after:to-neutral-900/80 -z-10 select-none">
                         <img
                             src="https://github.com/shadcn.png"
@@ -90,51 +105,192 @@ export default function CourseDetails() {
                     </div>
                 </div>
             </Card>
-            <div className="flex items-start space-x-6">
-                <div className="w-full">
+            <div className="flex flex-col lg:flex-row lg:items-start lg:space-x-6">
+                <div className="w-full min-w-0 order-2 lg:order-1 space-y-4 md:space-y-6">
                     <Card>
+                        <CardHeader>
+                            <h2 className="font-bold truncate">
+                                What you'll learn
+                            </h2>
+                        </CardHeader>
+                        <CardBody>
+                            <ul className="list-disc space-y-3 text-neutral-700 dark:text-neutral-300 text-sm">
+                                <li className="flex space-x-2">
+                                    <Check
+                                        size={16}
+                                        className="mt-0.5 flex-shrink-0"
+                                    />
+                                    <span>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. At, ad.
+                                    </span>
+                                </li>
+                                <li className="flex space-x-2">
+                                    <Check
+                                        size={16}
+                                        className="mt-0.5 flex-shrink-0"
+                                    />
+                                    <span>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing.
+                                    </span>
+                                </li>
+                                <li className="flex space-x-2">
+                                    <Check
+                                        size={16}
+                                        className="mt-0.5 flex-shrink-0"
+                                    />
+                                    <span>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Asperiores molestias
+                                        magnam sapiente.
+                                    </span>
+                                </li>
+                                <li className="flex space-x-2">
+                                    <Check
+                                        size={16}
+                                        className="mt-0.5 flex-shrink-0"
+                                    />
+                                    <span>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. At, ad.
+                                    </span>
+                                </li>
+                                <li className="flex space-x-2">
+                                    <Check
+                                        size={16}
+                                        className="mt-0.5 flex-shrink-0"
+                                    />
+                                    <span>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing.
+                                    </span>
+                                </li>
+                                <li className="flex space-x-2">
+                                    <Check
+                                        size={16}
+                                        className="mt-0.5 flex-shrink-0"
+                                    />
+                                    <span>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Asperiores molestias
+                                        magnam sapiente.
+                                    </span>
+                                </li>
+                            </ul>
+                        </CardBody>
+                    </Card>
+                    <Card className="overflow-hidden">
                         <CardHeader>
                             <h2 className="font-bold truncate">Lessons</h2>
                         </CardHeader>
                         <CardBody>
-                            <DetailLesson>
-                                <DetailLessonItem
-                                    title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-                                    link="/"
-                                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione quia minus, velit dicta voluptas veniam facere error dolorum et? Sapiente consequatur deleniti id ipsa enim numquam quibusdam labore adipisci?"
-                                    isLocked={false}
-                                />
-                                <DetailLessonItem
-                                    title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-                                    link="/"
-                                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione quia minus, velit dicta voluptas veniam facere error dolorum et? Sapiente consequatur deleniti id ipsa enim numquam quibusdam labore adipisci?"
-                                />
-                                <DetailLessonItem
-                                    title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-                                    link="/"
-                                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione quia minus, velit dicta voluptas veniam facere error dolorum et? Sapiente consequatur deleniti id ipsa enim numquam quibusdam labore adipisci?"
-                                />
-                                <DetailLessonItem
-                                    title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-                                    link="/"
-                                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione quia minus, velit dicta voluptas veniam facere error dolorum et? Sapiente consequatur deleniti id ipsa enim numquam quibusdam labore adipisci?"
-                                />
-                                <DetailLessonItem
-                                    title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-                                    link="/"
-                                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione quia minus, velit dicta voluptas veniam facere error dolorum et? Sapiente consequatur deleniti id ipsa enim numquam quibusdam labore adipisci?"
-                                />
-                                <DetailLessonItem
-                                    title="Lorem ipsum dolor sit amet, consectetur adipisicing."
-                                    link="/"
-                                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ratione quia minus, velit dicta voluptas veniam facere error dolorum et? Sapiente consequatur deleniti id ipsa enim numquam quibusdam labore adipisci?"
-                                />
-                            </DetailLesson>
+                            <div className="flex items-center flex-wrap space-y-2 space-x-2 -ml-2 -mt-2 mb-4">
+                                <div className="flex items-center space-x-2 text-xs font-medium first:ml-2 first:mt-2 h-6 px-3 rounded-full bg-neutral-100 dark:bg-neutral-950">
+                                    <Target size={16} />
+                                    <span>34 sections</span>
+                                </div>
+                                <div className="flex items-center space-x-2 text-xs font-medium first:ml-2 first:mt-2 h-6 px-3 rounded-full bg-neutral-100 dark:bg-neutral-950">
+                                    <CheckCircle size={16} />
+                                    <span>205 lessons</span>
+                                </div>
+                                <div className="flex items-center space-x-2 text-xs font-medium first:ml-2 first:mt-2 h-6 px-3 rounded-full bg-neutral-100 dark:bg-neutral-950">
+                                    <Timer size={16} />
+                                    <span>34h 4m</span>
+                                </div>
+                            </div>
+                            <div className="-mx-4 md:-mx-6 -mb-4 divide-y divide-neutral-100 dark:divide-neutral-800 border-t border-t-neutral-100 dark:border-t-neutral-800">
+                                <DetailLessonItem className="active">
+                                    <DetailLessonItemTrigger
+                                        label="Overview"
+                                        duration="13m"
+                                        lesson={7}
+                                    />
+                                    <DetailLessonItemLesson>
+                                        <DetailLessonItemLessonItem
+                                            title="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eum."
+                                            duration="45:08"
+                                            hasPreview={true}
+                                        />
+                                        <DetailLessonItemLessonItem
+                                            title="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eum."
+                                            duration="23:43"
+                                        />
+                                        <DetailLessonItemLessonItem
+                                            type="file"
+                                            title="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eum."
+                                            duration="10:32"
+                                        />
+                                    </DetailLessonItemLesson>
+                                </DetailLessonItem>
+                                <DetailLessonItem>
+                                    <DetailLessonItemTrigger
+                                        label="Overview test"
+                                        duration="13m"
+                                        lesson={7}
+                                    />
+                                    <DetailLessonItemLesson>
+                                        <DetailLessonItemLessonItem
+                                            type="file"
+                                            title="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eum."
+                                            duration="24:13"
+                                        />
+                                        <DetailLessonItemLessonItem
+                                            title="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eum."
+                                            duration="15:46"
+                                        />
+                                        <DetailLessonItemLessonItem
+                                            title="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, eum."
+                                            duration="34:45"
+                                        />
+                                    </DetailLessonItemLesson>
+                                </DetailLessonItem>
+                            </div>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <CardHeader>
+                            <h2 className="font-bold truncate">Requirements</h2>
+                        </CardHeader>
+                        <CardBody>
+                            <ul className="list-disc space-y-3 text-neutral-700 dark:text-neutral-300 text-sm pl-4">
+                                <li>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. At, ad.
+                                </li>
+                                <li>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Unde, accusamus?
+                                    <ul className="list-disc space-y-2 text-neutral-700 dark:text-neutral-300 pl-4 mt-2">
+                                        <li>
+                                            Lorem ipsum dolor sit amet
+                                            consectetur adipisicing elit.
+                                            Suscipit, corporis.
+                                        </li>
+                                        <li>
+                                            Lorem ipsum, dolor sit amet
+                                            consectetur adipisicing.
+                                        </li>
+                                        <li>Lorem ipsum dolor sit.</li>
+                                    </ul>
+                                </li>
+                                <li>Lorem ipsum dolor sit.</li>
+                            </ul>
                         </CardBody>
                     </Card>
                 </div>
-                <div className="w-96 flex-shrink-0 -mt-48 sticky top-20">
-                    <Card>
+                <div className="lg:w-80 xl:w-96 lg:flex-shrink-0 lg:-mt-48 mb-4 md:mb-6 lg:mb-0 lg:sticky lg:top-20 order-1 lg:order-2">
+                    <Card className="overflow-hidden">
+                        <button type="button" className="relative w-full">
+                            <img
+                                src="https://github.com/shadcn.png"
+                                alt=""
+                                className="w-full h-40 object-cover block"
+                            />
+                            <div className="absolute top-0 left-0 w-full h-full bg-black/30 hover:bg-black/40 transition-colors flex items-center justify-center text-neutral-300 hover:text-neutral-100">
+                                <Play size={32} />
+                            </div>
+                        </button>
                         <CardBody>
                             <div className="flex items-center space-x-3 mb-2">
                                 <h2 className="text-3xl font-semibold leading-tight text-indigo-600 dark:text-indigo-400">
